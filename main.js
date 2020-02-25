a174fea79880a61a00506378fcd589acac55fe94
Kevin Mancilla <kmancilla1@ucol.mx>
2020-02-25 13:34:30 -0600

    main modificado

--- main.js
+++ main.js
@@ -10,12 +10,20 @@ class Main{
 
     constructor(){
         this.cliente1 = new Cliente("Kevin Mancilla", "Av. Pino Suarez 121", "3121105426")
-        this.tiempo = new Tiempo(this.tiempo)
-        this.fecha = new Fecha(this.fecha)
-        this.direccion = new Direccion (this.direccion1)
-        this.producto = new Producto (this.producto)
+
+        this.tiempo1 = new Tiempo(3, 50, "am")
+        this.tiempo2 = new Tiempo(4, 50, "pm")
+
+        this.fecha = new Fecha(27,8,2001)
+
+        this.direccion = new Direccion ("Av. Pino Suarez", "121", "2A", "Fatima", "28050", "Colima", "Col")
+
+        this.producto1 = new Producto ("Pizza", "$55")
+        this.producto2 = new Producto ("Refresco", "$15")
+
         this.precio = new Precio(65.19)
-        this.elemento = new Elemento(this.elemento)
+
+        this.elemento = new Elemento ("Hamburguesa", "2x")
     }
 
 
@@ -24,39 +32,23 @@ class Main{
     }
     
     Tiempo(){
-        let hora = new Tiempo(3, 50, "am")
-        let hora2 = new Tiempo(4, 50, "pm")
-        console.log(`La hora esta programada para las ${hora.getFormato12()}`)
-        console.log(`La hora esta programada para las ${hora.getFormato24()}`)
-        console.log(`La hora esta programada para las ${hora2.getFormato12()}`)
-        console.log(`La hora esta programada para las ${hora2.getFormato24()}`)
+        console.log(`La hora esta programada para las ${this.tiempo1.getFormato12()}, que son las ${this.tiempo1.getFormato24()} en formato 24`)
+        console.log(`La hora esta programada para las ${this.tiempo2.getFormato12()} que son las ${this.tiempo2.getFormato24()} en formato 24`)
+
     }
 
     Fecha(){
-        let fechaP = new Fecha(27,8,2001)
-        console.log(`Son ${fechaP.getAños()} años`)
-        console.log(`Son ${fechaP.getMeses()} meses`)
-        console.log(`Son ${fechaP.getSemanas()} semanas`)
-        console.log(`Son ${fechaP.getDias()} dias `)
-        console.log(`El dia ${fechaP.getFecha()}`)
-        console.log(fechaP.getDiaFecha())
+        console.log(`Hoy es ${this.fecha.getDiaFecha()} ${this.fecha.getFecha()}. Son ${this.fecha.getAños()} años, ${this.fecha.getMeses()} meses, ${this.fecha.getSemanas()} semanas, ${this.fecha.getDias()} dias. `)
     }
 
-    Direccion(){
-        let direccion = new Direccion ("Av. Pino Suarez", "121", "2A", "Fatima", "28050", "Colima", "Col")
-        console.log(direccion.getFormatoCorto())
-        console.log(direccion.getFormatoLargo())
+    Direccion(){    
+        console.log(`La direccion corta es: ${this.direccion.getFormatoCorto()} y la direccion larga es: ${this.direccion.getFormatoLargo()}`)
     }
 
     Producto(){
-        let producto1 = new Producto ("Pizza", "$55")
-        let producto2 = new Producto ("Hamburguesa", "$45")
-        let producto3 = new Producto ("Refresco", "$15")
-        let producto4 = new Producto ("Agua", "$10")
-        console.log(producto1.getDescripcion())
-        console.log(producto2.getDescripcion())
-        console.log(producto3.getDescripcion())
-        console.log(producto4.getDescripcion())
+        console.log(this.producto1.getDescripcion())
+        console.log(this.producto2.getDescripcion())
+
     }
 
     Precio(){
@@ -64,8 +56,7 @@ class Main{
     }
 
     Elemento(){
-        let elemento1 = new Elemento ("Hamburguesa", "2x")
-        console.log(elemento1.getDescripcion())
+        console.log(this.elemento.getDescripcion())
     }
     }
 
